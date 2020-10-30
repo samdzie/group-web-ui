5e9de16cc25406cdea3d182f034f13aca949441d
Sam Dziewietin <ssdz10@gmail.com>
2020-10-30 13:15:05 -0400

    Add endpoints for Vue UI

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,7 @@ import random
 import string
 from datetime import datetime, timedelta
 import requests
-from flask import Flask, jsonify, make_response, url_for
+from flask import Flask, jsonify, make_response, send_from_directory, url_for
 
 
 app = Flask(__name__)
@@ -13,9 +13,27 @@ app.config['EVENT_SERVER_HOST'] = 'http://127.0.0.1:5002'
 
 
 @app.route('/')
-def simple_homepage():
-    """Return a simple homepage while Vue integration is not complete."""
-    return '<h1>Group Web UI</h1><p>Under construction! Check back later!</p>'
+def send_homepage():
+    """Send index.html from the built Vue app."""
+    return send_from_directory('dist', 'index.html')
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    """Send JavaScript files from the built Vue app."""
+    return send_from_directory('dist/js', path)
+
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    """Send CSS files from the built Vue app."""
+    return send_from_directory('dist/css', path)
+
+
+@app.route('/favicon.ico')
+def send_favicon():
+    """Send the favicon from the built Vue app."""
+    return send_from_directory('dist', 'favicon.ico')
 
 
 @app.route('/api')
